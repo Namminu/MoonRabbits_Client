0518b04b089e0520385c670fad8baebbde104e42
Namminu <mvpalsdn771@gmail.com>
2025-02-21 14:15:38 +0900

    feat : ItemSlot Index 값 추가

--- Assets/Scripts/Town/UI Scripts/Inventory CS/Moveable Inven.cs
+++ Assets/Scripts/Town/UI Scripts/Inventory CS/Moveable Inven.cs
@@ -6,21 +6,16 @@ using UnityEngine.UI;
 
 public class MoveableInven : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
-    [SerializeField]
-    private Transform targetUI;
+    [SerializeField] private Transform targetUI;
 
-    [SerializeField]
-    private GameObject DecomUI;
+    [SerializeField] private GameObject DecomUI;
 
     [Space]
-    [SerializeField]
-    private Button btn_Sort;
+    [SerializeField] private Button btn_Sort;
 
-    [SerializeField]
-    private Button btn_Decom;
+    [SerializeField] private Button btn_Decom;
 
-    [SerializeField]
-    private Button btn_Close;
+    [SerializeField] private Button btn_Close;
 
     // 인벤 UI의 위치 이동을 위한 변수
     private Vector2 beginPos;
