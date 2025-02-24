9a7839a3d0105e6ec10d2ba987e2d0508b60f642
wakelight23 <rlghks0223@gmail.com>
2025-02-24 11:06:04 +0900

    Fix : 아이템 관련 코드 수정

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+    // 로컬 JSON 파일 경로 예시 (StreamingAssets 폴더)
+    private string jsonFilePath => Path.Combine(Application.streamingAssetsPath, "./ItemAssets/itemData.json");
     private int itemCode;
     private Image itemImage;
     private ItemData itemData;
@@ -13,4 +17,22 @@ public class Item : MonoBehaviour
     public Image GetItemImage() { return itemImage; } 
 
     public ItemData GetItemData() { return itemData; }
+
+    public IEnumerator SendItemData()
+    {
+        // JSON 파일 읽기
+        string jsonData;
+        try {
+            jsonData = File.ReadAllText(jsonFilePath);
+        } catch(Exception e) {
+            Debug.LogError("JSON 파일 읽기 에러: " + e.Message);
+            yield break;
+        }
+
+        // protobuf 메시지 생성
+        // ItemPacket itemPacket = new ItemPacket
+        // {
+        //     JsonData = jsonData
+        // };
+    }
 }
