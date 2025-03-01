e2cb0910c6b4c99576c49906b4ade76ea0a3746a
wjdxo2412 <wjdxo2412@gmail.com>
2025-03-01 16:56:53 +0900

    feat : 이팩트 매니저 오브젝트 붙어서 생성되는 기능 추가

--- Assets/Scripts/Battle/EffectManager.cs
+++ Assets/Scripts/Battle/EffectManager.cs
@@ -19,20 +19,29 @@ public class EffectManager : MonoBehaviour
         }
     }
 
+    //일정시간 동안 실행되어야하는 특정 좌표가 있는 이팩트
+
     public GameObject CreateTemporaryEffect(string effectName, Vector3 position, Quaternion rotation, float duration)
     {
         return CreateEffect(effectName, position, rotation, true, duration);
     }
 
+    // 이건 일정 시간 동안 실행되어야 하는 부모가 있는 이팩트
+    public GameObject CreateTemporaryEffect(string effectName, Transform parent, float duration)
+    {
+        return CreateEffect(effectName, default, default, true, duration, parent);
+    }
+
+    // 이건 지속적으로 실행되어야 하는 이펙트 자연물 같은 느낌
     public GameObject CreatePersistentEffect(string effectName, Vector3 position, Quaternion rotation)
     {
         return CreateEffect(effectName, position, rotation, false, 0);
     }
 
-    private GameObject CreateEffect(string effectName, Vector3 position, Quaternion rotation, bool isTemporary, float duration)
+    private GameObject CreateEffect(string effectName, Vector3 position, Quaternion rotation, bool isTemporary, float duration, Transform parent = null)
     {
         // ResourceManager의 Instantiate 메서드를 사용하여 이펙트를 생성합니다.
-        GameObject effectInstance = ResourceManager.Instance.Instantiate("Effects", effectName, position, rotation);
+        GameObject effectInstance = ResourceManager.Instance.Instantiate("Effects", effectName, position, rotation, parent);
 
         if (effectInstance != null)
         {
