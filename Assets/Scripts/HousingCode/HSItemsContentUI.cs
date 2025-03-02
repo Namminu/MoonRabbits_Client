0cfcc0a8611dced513cd570122d1ad11f2157c20
Namminu <mvpalsdn771@gmail.com>
2025-03-03 00:40:56 +0900

    feat : 하우징 시스템 구현중
    1. 그리드 시스템, 쉐이더 추가
    2. UI버튼 상호작용 테스트 확인
    3. 클릭 및 씬 배치 작동 확인
    - 현재 테스트 코드 및 테스트 DB 들어가있어서, 추후 json 파일 및 HSItem 연동 필요

--- Assets/Scripts/HousingCode/HSItemsContentUI.cs
+++ Assets/Scripts/HousingCode/HSItemsContentUI.cs
@@ -6,5 +6,9 @@ public class HSItemsContentUI : MonoBehaviour
 {
 	[SerializeField] private GameObject HSItemPrefab;
 
-
+	private async void Awake()
+	{
+		/* Json 파일 로드 임시 코드 */
+		await ItemDataLoader.GenerateAllItems();
+	}
 }
