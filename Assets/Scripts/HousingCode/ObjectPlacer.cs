9ed8c5018e16f6080ff04b9d3c6f3e4e6d1846cf
wakelight23 <rlghks0223@gmail.com>
2025-03-12 13:51:53 +0900

    BugFix : 하우징 시스템 버그 해결
    버그 수정
    - 설치된 가구를 제거할 때 발생하던 버그가 해결되었습니다,
    - 가구 인벤토리 리스트가 이중으로 추가되던 현상을 수정했습니다,
    - 가구 데이터의 Grid가 일치하지 않던 현상을 수정했습니다.

--- Assets/Scripts/HousingCode/ObjectPlacer.cs
+++ Assets/Scripts/HousingCode/ObjectPlacer.cs
@@ -62,8 +62,8 @@ public class ObjectPlacer : MonoBehaviour
         if (placedGameObject.Count <= gameObjectIndex || placedGameObject[gameObjectIndex] == null)
             return;
         Destroy(placedGameObject[gameObjectIndex]);
-		//placedGameObject[gameObjectIndex] = null;
-		placedGameObject.RemoveAt(gameObjectIndex);
+        placedGameObject[gameObjectIndex] = null;
+        //placedGameObject.RemoveAt(gameObjectIndex);
 	}
 
     #region 이전 코드
